fc6243947996ec759946283a00c6cd50f23ceee7
Julia <iulia.golicova@gmail.com>
2020-08-21 17:22:00 -0400

    functions started

---
+++ functions/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for develoer tea time...");
+        System.out.println("Type in a random word and press enter to start developer tea time.");
+        Scanner input = new Scanner(System.in);
+        // do not need to use it so we don't need to save it
+        input.next();
+
+        System.out.println("It's developer tea time!");
+    }
+
+    public static void main (String args[]) {
+
+    }
+}
